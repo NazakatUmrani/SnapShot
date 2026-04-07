1523213bc29c422571ecf14bda21cc521c09fe13
Nazakat Umrani - NixOS Linux <unazakat70@gmail.com>
2026-04-07 21:55:40 +0500

    react-firebase-hook for userState and loading

--- src/components/ProtectedRoutes/index.tsx
+++ src/components/ProtectedRoutes/index.tsx
@@ -1,12 +1,17 @@
-import { useUserAuth } from "@/context/userAuthContext"
 import * as React from "react"
 import { Navigate, Outlet, useLocation } from "react-router-dom"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { firebaseAuth } from "@/firebaseConfig"
 
 interface IProtectedRoutesProps {}
 
 const ProtectedRoutes: React.FunctionComponent<IProtectedRoutesProps> = () => {
-  const { user } = useUserAuth();
-  const location = useLocation()
+  const [user, loading] = useAuthState(firebaseAuth);
+  const location = useLocation();
+
+  if(loading) {
+    return <div>Loading...</div>
+  }
 
   return user ? (
     <Outlet />
