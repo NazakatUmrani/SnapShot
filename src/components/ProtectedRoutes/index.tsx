9ca183c36ffecc9b0322f1eced6f201487c5d823
Nazakat Umrani - NixOS Linux <unazakat70@gmail.com>
2026-04-07 21:48:22 +0500

    Auth user logic added

--- src/components/ProtectedRoutes/index.tsx
+++ src/components/ProtectedRoutes/index.tsx
@@ -1,16 +1,18 @@
-import * as React from 'react';
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { useUserAuth } from "@/context/userAuthContext"
+import * as React from "react"
+import { Navigate, Outlet, useLocation } from "react-router-dom"
 
-interface IProtectedRoutesProps {
+interface IProtectedRoutesProps {}
+
+const ProtectedRoutes: React.FunctionComponent<IProtectedRoutesProps> = () => {
+  const { user } = useUserAuth();
+  const location = useLocation()
+
+  return user ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" state={{ from: location }} />
+  )
 }
 
-const ProtectedRoutes: React.FunctionComponent<IProtectedRoutesProps> = (props) => {
-  const isAuthenticated:boolean = false;
-    const location = useLocation();
-
-    return isAuthenticated ? 
-        (<Outlet />) : 
-        (<Navigate to='/login' state={{ from: location }}/>);
-};
-
-export default ProtectedRoutes;
+export default ProtectedRoutes
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,36 @@
+import { Button } from '@/components/ui/button';
+import { useUserAuth } from '@/context/userAuthContext';
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 interface IHomeProps {
 }
 
 const Home: React.FunctionComponent<IHomeProps> = (props) => {
+    const { logOut } = useUserAuth();
+    const navigate = useNavigate();
+
+    const handleSignOut = async () => {
+        try {
+            logOut();
+            toast.success("Signout successful");
+            navigate("/login");
+        } catch (error) {
+            console.error(error);
+            toast.error("Signout failed");
+        }
+    }
+
     return (
-        <div>
-        Home
-        </div>
+        <>
+            <div>
+                Home
+            </div>
+            <Button
+                onClick={handleSignOut}
+            >Signout</Button>
+        </>
     );
 };
 
